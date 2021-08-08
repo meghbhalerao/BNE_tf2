24abed9ff8419859bb6031f05fa22006deb7922b
megh <megh.bhalerao@gmail.com>
2021-08-08 09:34:13 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+python bne.py --model models/BNE_SGw --fe ./embeddings/BioEmb/Emb_SGw.txt --fi names.txt --fo output_BNE_SGw.txt
+#python bne.py --model models/BNE_SGsc --fe ./embeddings/BioEmb/Emb_SGsc.txt --fi names.txt --fo output_BNE_SGsc.txt
